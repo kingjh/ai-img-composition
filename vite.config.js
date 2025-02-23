61a2ae187c381f6072c1840e2ddcd763f835baa8
John Wong <j_jo@sohu.com>
2025-02-24 00:07:33 +0800

    chord: init

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,15 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   // 设置基础路径为 /项目名/
-  base: '/ai-img-composition/',
+  base: '/ai-fortune-telling/',
   plugins: [vue()],
   build: {
     // 输出目录设置为 ../docs
     outDir: path.resolve(__dirname, 'docs'),
     // 静态资源直接输出到 docs 文件夹下
-    assetsDir: ''
+    assetsDir: '',
+    rollupOptions: {
+      external: ['@vuepic/vue-datepicker'],
+    }
   }
 });
